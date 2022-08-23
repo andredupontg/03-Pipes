d67e99d61036aa966f3bfbdf23066a2d4df0cbad
André Dupont <andredupontg@hotmail.com>
2022-08-22 23:05:41 -0500

    ADD: Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '03-Pipes';
+  name: string = "B3dserk Studios";
+  numberArray: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  PI:number = Math.PI;
+  percentage: number = 0.234;
+  salary: number = 1234.5;
+  hero:any = {
+    fullName: "Barry Allen",
+    aka: "Flash",
+    age: 37,
+    address: {
+      city: "Central City",
+      country: "United States",
+    }
+  };
+  promise = new Promise<string>(resolve => {
+    setTimeout(() => {resolve("data arrived!")}, 3500);
+  });
+  date: Date = new Date();
 }
