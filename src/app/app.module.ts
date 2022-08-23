2b13fc10e23d5c333df4b74e8eb828b24307bf1b
André Dupont <andredupontg@hotmail.com>
2022-08-22 23:17:32 -0500

    ADD: Language Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {registerLocaleData} from "@angular/common";
+import localeEs from "@angular/common/locales/es";
+import localeFr from "@angular/common/locales/fr";
+
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
@@ -12,7 +18,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: "es"}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
